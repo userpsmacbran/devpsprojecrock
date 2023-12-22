1c6bbaa2aeb02df04ea84b8bd77cc63057f6e417
Alejandro <alejandroaml0528@gmail.com>
2023-12-22 14:16:48 -0400

    frontend changes two

---
+++ Frontend/src/auth/AuthProvider.jsx
@@ -0,0 +1,67 @@
+import { useContext, createContext, useState, useEffect } from "react";
+
+const AuthContext = createContext({
+  isAuthenticated: false,
+  getAccessToken: () => {},
+  saveUser: (userData) => {},
+  checkTokenExpiration: () => {},
+});
+
+export function AuthProvider({ children }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    window.localStorage.getItem("token" ?? false)
+  );
+
+  const [accessToken, setAccessToken] = useState("");
+
+  useEffect(() => {
+    checkTokenExpiration();
+    checkAuth();
+  }, []);
+
+  async function checkAuth() {
+    if (localStorage.getItem("token")) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }
+
+  function getAccessToken() {
+    return accessToken;
+  }
+
+  function saveUser(userData) {
+    setAccessToken(userData.token);
+    localStorage.setItem("token", JSON.stringify(userData.token));
+    localStorage.setItem("tokenExpiration", new Date(userData.tokenExpiration));
+    setIsAuthenticated(true);
+  }
+
+  function checkTokenExpiration() {
+    console.log("checkToken");
+    const expirationDate = new Date(localStorage.getItem("tokenExpiration"));
+    if (expirationDate < new Date()) {
+      // Token expirado, limpia el almacenamiento
+      localStorage.removeItem("token");
+      localStorage.removeItem("tokenExpiration");
+      setAccessToken("");
+      setIsAuthenticated(false);
+    }
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        getAccessToken,
+        saveUser,
+        checkTokenExpiration,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => useContext(AuthContext);
---
+++ Frontend/src/components/PublicRoute.jsx
@@ -0,0 +1,16 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../auth/AuthProvider";
+export default function PublicRoute() {
+  const { isAuthenticated } = useAuth();
+
+  console.log(isAuthenticated);
+  if (isAuthenticated) {
+    return <Navigate to="/dashboard" />;
+  }
+
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+}
